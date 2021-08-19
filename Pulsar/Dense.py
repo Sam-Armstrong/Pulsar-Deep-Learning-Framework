f6f07311188b9de9035ec94292c160528c355dea
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2021-08-19 04:55:55 +0100

    Implemented backpropagation using stochastic gradient descent

--- Pulsar/Dense.py
+++ Pulsar/Dense.py
@@ -11,10 +11,12 @@ import numpy as np
 
 class Dense:
     
-    def __init__(self, Nin, Nout, initialization = 'He', activation = 'relu') -> None:
+    def __init__(self, Nin, Nout, initialization = 'He', activation = 'relu', learning_rate = 0.01) -> None:
         self.Nin = Nin
         self.Nout = Nout
         self.biases = np.zeros((Nout))
+        #self.biases = np.random.uniform(0, 1, Nin)
+        self.lr = learning_rate
 
         if initialization == 'He':
             self.weights = Initialization().He_init(Nin, Nout)
@@ -28,9 +30,39 @@ class Dense:
 
         # Calculates the layer output prior to applying the activation function
         h = np.transpose(np.matmul(self.weights, np.transpose(batch)))
-        h += self.biases[np.newaxis,:] # Adds the biases
+        h = np.add(h, self.biases[np.newaxis,:]) # Adds the biases
         
         # TODO the selected activation function needs to be used here
         y = a.ReLU(h) # Applies the activation function
 
         return y
+
+    def backpropagate(self, batch, batch_labels = None, next_layer_weights = None, next_layer_grad = None):
+        a = Activation()
+
+
+        # Calculates the layer output prior to applying the activation function
+        h = np.transpose(np.matmul(self.weights, np.transpose(batch)))
+        h = np.add(h, self.biases[np.newaxis,:]) # Adds the biases
+        
+        # TODO the selected activation function needs to be used here
+        y = a.ReLU(h) # Applies the activation function
+
+        derivation_matrix = a.derivationReLU(h)
+
+        if next_layer_weights == None and next_layer_grad == None:
+            # Calculates the error signal
+            e_n = np.subtract(batch_labels, y)
+            # Calculates the local gradient of each neuron
+            delta = np.multiply(e_n, derivation_matrix)
+
+        else:
+            delta = np.multiply(np.matmul(next_layer_weights.T, next_layer_grad.T).T, derivation_matrix)
+
+        self.weights += (np.matmul(delta.T, batch) * self.lr) / len(batch)
+        self.biases += (sum(delta) * self.lr) / len(batch)
+        print(len(batch))
+
+        return y, delta, self.weights
+
+#print(Dense(3,3).forwardPass(np.array([[-1, 2, 3], [4, -5, 6]], np.int32)))
--- Pulsar/Pulsar.py
+++ Pulsar/Pulsar.py
@@ -5,5 +5,58 @@ Date: 2021
 Description: The main class that is used for implementing neural networks.
 """
 
+from Dense import Dense
+import numpy as np
+
+# Normalizes a set of data
+def NormalizeInput(x):
+    return (x - np.min(x)) / (np.max(x) - np.min(x))
+
 class Pulsar:
-    pass
+    
+    def __init__(self, optimizer = 'sgd', learning_rate = 0.01, batch_size = 200, epochs = 100, 
+                 regularization = 'l1', loss = 'mean-squared') -> None:
+        self.optimizer = optimizer
+        self.lr = learning_rate
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.regularization = regularization
+        self.loss_function = loss
+        self.layers = list()
+
+    def dense(self, Nin, Nout):
+        self.layers.append(Dense(Nin, Nout))
+
+    def train(self, training_data, training_labels):
+        training_data = NormalizeInput(training_data) # Normalizes the input training data
+        Ntrain = len(training_data)
+        Nlayers = len(self.layers)
+
+        for n in range(self.epochs):
+            batch_number = 1
+            dW = 0
+            db = 0
+
+            shuffled_idxs = np.random.permutation(Ntrain)
+
+            # Loops through all the batches in the training set
+            while Ntrain - (batch_number * self.batch_size) >= self.batch_size:
+                start_index = batch_number * self.batch_size
+                batch = np.zeros((self.batch_size, 784))
+                batch_labels = np.zeros((self.batch_size, 26))
+
+                idxs = shuffled_idxs[start_index:(start_index + self.batch_size)]
+
+                # Collects the next batch
+                for i in range(len(idxs)):
+                    batch[i] = training_data[idxs[i]]
+                    batch_labels[i] = training_labels[idxs[i]]
+
+                layer = self.layers[Nlayers - 1]
+                current_batch, gradient, W = layer.backpropagate(batch, batch_labels = batch_labels)
+                
+                for i in range(Nlayers):
+                    layer = self.layers[Nlayers - (i + 1)]
+                    current_batch, gradient, W = layer.backpropagate(current_batch, next_layer_weights = W, next_layer_grad = gradient)
+
+                batch_number += 1
