Fixed backpropagation

Pulsar/Dense.py
@@ -40,7 +40,6 @@ class Dense:
     def backpropagate(self, batch, batch_labels = None, next_layer_weights = None, next_layer_grad = None):
         a = Activation()
 
-
         # Calculates the layer output prior to applying the activation function
         h = np.transpose(np.matmul(self.weights, np.transpose(batch)))
         h = np.add(h, self.biases[np.newaxis,:]) # Adds the biases
@@ -50,19 +49,17 @@ class Dense:
 
         derivation_matrix = a.derivationReLU(h)
 
-        if next_layer_weights == None and next_layer_grad == None:
+        try:
+            delta = np.multiply(np.transpose(np.matmul(np.transpose(next_layer_weights), np.transpose(next_layer_grad))), derivation_matrix)
+        except:
             # Calculates the error signal
             e_n = np.subtract(batch_labels, y)
             # Calculates the local gradient of each neuron
             delta = np.multiply(e_n, derivation_matrix)
 
-        else:
-            delta = np.multiply(np.matmul(next_layer_weights.T, next_layer_grad.T).T, derivation_matrix)
-
         self.weights += (np.matmul(delta.T, batch) * self.lr) / len(batch)
         self.biases += (sum(delta) * self.lr) / len(batch)
-        print(len(batch))
-
+        
         return y, delta, self.weights
 
 #print(Dense(3,3).forwardPass(np.array([[-1, 2, 3], [4, -5, 6]], np.int32)))

Pulsar/Pulsar.py
@@ -14,7 +14,7 @@ def NormalizeInput(x):
 
 class Pulsar:
     
-    def __init__(self, optimizer = 'sgd', learning_rate = 0.01, batch_size = 200, epochs = 100, 
+    def __init__(self, optimizer = 'sgd', learning_rate = 0.01, batch_size = 200, epochs = 5, 
                  regularization = 'l1', loss = 'mean-squared') -> None:
         self.optimizer = optimizer
         self.lr = learning_rate
@@ -25,7 +25,7 @@ class Pulsar:
         self.layers = list()
 
     def dense(self, Nin, Nout):
-        self.layers.append(Dense(Nin, Nout))
+        self.layers.append(Dense(Nin, Nout, learning_rate = self.lr))
 
     def train(self, training_data, training_labels):
         training_data = NormalizeInput(training_data) # Normalizes the input training data
@@ -43,7 +43,7 @@ class Pulsar:
             while Ntrain - (batch_number * self.batch_size) >= self.batch_size:
                 start_index = batch_number * self.batch_size
                 batch = np.zeros((self.batch_size, 784))
-                batch_labels = np.zeros((self.batch_size, 26))
+                batch_labels = np.zeros((self.batch_size, 10))
 
                 idxs = shuffled_idxs[start_index:(start_index + self.batch_size)]
 
@@ -52,11 +52,23 @@ class Pulsar:
                     batch[i] = training_data[idxs[i]]
                     batch_labels[i] = training_labels[idxs[i]]
 
+                # Updates the final layer
                 layer = self.layers[Nlayers - 1]
-                current_batch, gradient, W = layer.backpropagate(batch, batch_labels = batch_labels)
+                x = self.getLayersOutput(batch, self.layers[0:Nlayers - 1])
+                current_batch, gradient, W = layer.backpropagate(x, batch_labels = batch_labels)
                 
-                for i in range(Nlayers):
-                    layer = self.layers[Nlayers - (i + 1)]
-                    current_batch, gradient, W = layer.backpropagate(current_batch, next_layer_weights = W, next_layer_grad = gradient)
-
+                i = Nlayers - 2
+                # Updates the hidden layers
+                while i >= 0:
+                    layer = self.layers[i]
+                    x = self.getLayersOutput(batch, self.layers[0:i])
+                    current_batch, gradient, W = layer.backpropagate(x, next_layer_weights = W, next_layer_grad = gradient)
+                    i -= 1
+                    
                 batch_number += 1
+
+    def getLayersOutput(self, batch, layers):
+        x = batch
+        for l in layers:
+            x = l.forwardPass(x)
+        return x